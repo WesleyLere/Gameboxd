646baad7fb7137ce1f145b09bdb624089a87ccec
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-10 11:31:45 -0600

    testing

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(routes);
 
