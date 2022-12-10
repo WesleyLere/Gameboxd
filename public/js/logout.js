0a3459a9d57d7785eda9e16b456565352c479536
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-10 14:19:19 -0600

    working on logout and showing username on profile

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,5 @@
 const logout = async () => {
-  const response = await fetch('/controllers/api/userRoutes', {
+  const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
   });
