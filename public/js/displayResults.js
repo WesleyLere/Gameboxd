849d2245bdad6f980cae9d2346749d81913f7537
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-12 19:53:08 -0600

    search page is pretty now

--- public/js/displayResults.js
+++ public/js/displayResults.js
@@ -5,10 +5,12 @@ function displayData(){
     let data = JSON.parse(localStorage.getItem('searchResult'))
     let searchGames = ''
     for (let i = 0; i < data.length; i++) { 
-        aLiString += `<div class="justify-center items-center border-4 border-gray-100 mx-auto m-5">
-        <h2 class="text-3xl p-10 tedxt-slate-700 container flex flex-wrap items-center justify-center mx-auto">${data[i].title}
+        searchGames += `<div class="flex flex-wrap justify-center text-center item-center w-96 h-auto items-center border-4 m-10 p-5">
+        <h2 class="text-2xl flex flex-wrap justify-center text-center item-center m-5">${data[i].title}
         </h2>
-        <img src="${data[i].imgUrl}" alt="" class=" mx-auto m-5 flex-wrap flex row justify-center items-center hover:scale-110 transition duration-200 ease-in-out cursor-pointer">
+        <a href="/game/${data[i].appId}">
+        <img src="${data[i].imgUrl}" alt=""  class="flex flex-wrap justify-center items-center hover:scale-110 transition duration-200 ease-in-out cursor-pointer hover:shadow-lg hover:shadow-amber-400">
+        </a>
       </div>`
     }
     targetElement.innerHTML = searchGames
@@ -17,5 +19,4 @@ function displayData(){
 }
 
 displayData() 
-
 
