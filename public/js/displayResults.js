98a89e2d8b5a067f31b8c40b02040a4acde5377f
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-12 18:24:35 -0600

    fixing displayJS

--- public/js/displayResults.js
+++ public/js/displayResults.js
@@ -9,13 +9,8 @@ function displayData(){
     for (let i = 0; i < data.length; i++) { 
         aLiString += `<div class="justify-center items-center border-4 border-gray-100 mx-auto m-5">
         <h2 class="text-3xl p-10 tedxt-slate-700 container flex flex-wrap items-center justify-center mx-auto">${data[i].title}
-        searchGames += `<div class="flex flex-wrap justify-center items-center border-4 border-gray-100 mx-auto m-5">
-        <h2 class="text-2xl p-5 mt-5 text-slate-700 container flex items-center justify-center">${data[i].title}
         </h2>
         <img src="${data[i].imgUrl}" alt="" class=" mx-auto m-5 flex-wrap flex row justify-center items-center hover:scale-110 transition duration-200 ease-in-out cursor-pointer">
-        <a href="/game/${data[i].appId}">
-        <img src="${data[i].imgUrl}" alt=""  class=" mx-auto m-5 flex-wrap flex row justify-center items-center hover:scale-110 transition duration-200 ease-in-out cursor-pointer hover:shadow-md hover:shadow-amber-400">
-        </a>
       </div>`
     }
     targetElement.innerHTML = searchGames
@@ -26,23 +21,3 @@ function displayData(){
 displayData() 
 
 
-    gameSearched=gameSearchedInput.value
- 
-    gameSearchURL = 'https://steam2.p.rapidapi.com/search/' + game + '/page/1', options
- 
-    console.log(gameSearched)
- 
-    setGame()
- 
-
-
- 
-// first get data from local storage 
-// loop through data and target element inside search page.handlebars 
-// clear out key searchResult from local storage 
-//  hResult from local storage 
-//  hResult from local storage 
-//  hResult from local storage 
-//  hResult from local storage 
-//  hResult from local storage 
-//  
