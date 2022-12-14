469fbef3831b7f8213d8b7c8c80630968fb79bc8
Minh Truong <mptruong49@gmail.com>
2022-12-13 17:50:04 -0700

    force: false

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(routes);
 
-sequelize.sync({force: true}).then(() => {
+sequelize.sync({force: false}).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 // change back to force: false
