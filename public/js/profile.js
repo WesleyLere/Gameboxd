c3d37815ef45d0b5cb908cdead178987eb60d48f
Minh Truong <mptruong49@gmail.com>
2022-12-13 20:30:17 -0700

    game reviews working and profile not

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,17 +1,25 @@
-const { UserGame } = require("../../models");
+// const { UserGame } = require("../../models");
 
-function getprofilegames() {
-    const gameId = UserGame
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '39fcf7cf62mshb1cac5ccb20e1eep1cf83ejsnd5589ee92c81',
-            'X-RapidAPI-Host': 'steam2.p.rapidapi.com'
-        }
-    };
+// function getprofilegames() {
+//     const gameId = UserGame
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': '39fcf7cf62mshb1cac5ccb20e1eep1cf83ejsnd5589ee92c81',
+//             'X-RapidAPI-Host': 'steam2.p.rapidapi.com'
+//         }
+//     };
     
-    fetch('https://steam2.p.rapidapi.com/appDetail/730', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+//     fetch('https://steam2.p.rapidapi.com/appDetail/730', options)
+//         .then(response => response.json())
+//         .then(response => console.log(response))
+//         .catch(err => console.error(err));
+// }
+
+async function getUser() {
+   const userData = await fetch('/api/users/')
+   const user = await userData.json();
+   console.log(user);
 }
+
+getUser();
